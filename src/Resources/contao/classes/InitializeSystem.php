4544de9903d60c595f543ddac99f09d37f88a0de
Marko Cupic <m.cupic@gmx.ch>
2019-03-09 11:18:51 +0100

    Hotfix
    Check if notification center is installed, otherwise the system crashes

--- src/Resources/contao/classes/InitializeSystem.php
+++ src/Resources/contao/classes/InitializeSystem.php
@@ -27,47 +27,50 @@ class InitializeSystem
      */
     public function initializeSystem()
     {
-        // Create default notification
-        $objNotification = Notification::findOneByType('default_email');
-        if ($objNotification === null)
+        if (Database::getInstance()->tableExists('tl_nc_notification'))
         {
-            $set = array(
-                'type'   => 'default_email',
-                'title'  => 'Standard E-Mail (nur mit Platzhaltern)',
-                'tstamp' => time()
-            );
-            $oInsertStmt = Database::getInstance()->prepare('INSERT into tl_nc_notification %s')->set($set)->execute();
+            // Create default notification
+            $objNotification = Notification::findOneByType('default_email');
+            if ($objNotification === null)
+            {
+                $set = array(
+                    'type'   => 'default_email',
+                    'title'  => 'Standard E-Mail (nur mit Platzhaltern)',
+                    'tstamp' => time()
+                );
+                $oInsertStmt = Database::getInstance()->prepare('INSERT into tl_nc_notification %s')->set($set)->execute();
 
-            $set = array(
-                'pid'            => $oInsertStmt->insertId,
-                'tstamp'         => time(),
-                'title'          => 'Standard Nachricht',
-                'gateway'        => 1,
-                'gateway_type'   => 'email',
-                'email_priority' => 3,
-                'email_template' => 'mail_default',
-                'published'      => 1
-            );
-            $oInsertStmt2 = Database::getInstance()->prepare('INSERT into tl_nc_message %s')->set($set)->execute();
+                $set = array(
+                    'pid'            => $oInsertStmt->insertId,
+                    'tstamp'         => time(),
+                    'title'          => 'Standard Nachricht',
+                    'gateway'        => 1,
+                    'gateway_type'   => 'email',
+                    'email_priority' => 3,
+                    'email_template' => 'mail_default',
+                    'published'      => 1
+                );
+                $oInsertStmt2 = Database::getInstance()->prepare('INSERT into tl_nc_message %s')->set($set)->execute();
 
-            $set = array(
-                'pid'                  => $oInsertStmt2->insertId,
-                'tstamp'               => time(),
-                'gateway_type'         => 'email',
-                'language'             => 'de',
-                'fallback'             => '1',
-                'recipients'           => '##send_to##',
-                'attachment_tokens'    => '#attachment_tokens##',
-                'email_sender_name'    => '##email_sender_name##',
-                'email_sender_address' => '##email_sender_email##',
-                'email_recipient_cc'   => '##recipient_cc##',
-                'email_recipient_bcc'  => '##recipient_bcc##',
-                'email_replyTo'        => '##reply_to##',
-                'email_subject'        => '##email_subject##',
-                'email_mode'           => 'extOnly',
-                'email_text'           => '##email_text##'
-            );
-            $oInsertStmt3 = Database::getInstance()->prepare('INSERT into tl_nc_language %s')->set($set)->execute();
+                $set = array(
+                    'pid'                  => $oInsertStmt2->insertId,
+                    'tstamp'               => time(),
+                    'gateway_type'         => 'email',
+                    'language'             => 'de',
+                    'fallback'             => '1',
+                    'recipients'           => '##send_to##',
+                    'attachment_tokens'    => '#attachment_tokens##',
+                    'email_sender_name'    => '##email_sender_name##',
+                    'email_sender_address' => '##email_sender_email##',
+                    'email_recipient_cc'   => '##recipient_cc##',
+                    'email_recipient_bcc'  => '##recipient_bcc##',
+                    'email_replyTo'        => '##reply_to##',
+                    'email_subject'        => '##email_subject##',
+                    'email_mode'           => 'extOnly',
+                    'email_text'           => '##email_text##'
+                );
+                $oInsertStmt3 = Database::getInstance()->prepare('INSERT into tl_nc_language %s')->set($set)->execute();
+            }
         }
     }
 
